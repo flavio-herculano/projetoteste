5155ab6753c43659e496d8952a32a92752d27621
Flávio Herculano <flavio.hercullano@gmail.com>
2022-05-13 08:46:21 -0300

    Adicionado conexão com banco de dados

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,40 @@
-import styled from "styled-components";
-import Link from "next/link";
+import clientPromise from './lib/mongodb'
 
-const Title = styled.h1`
-	font-size: 50px;
-	color: ${({ theme }) => theme.colors.primary};
-`;
+export default function Home({ isConnected }) {
+  return (
+    <>
+        {isConnected ? (
+          <h2 className="subtitle">You are connected to MongoDB</h2>
+        ) : (
+          <h2 className="subtitle">
+            You are NOT connected to MongoDB. Check the <code>README.md</code>{' '}
+            for instructions.
+          </h2>
+        )}
+    
+    </>
+  )
+}
 
-export default function Home() {
-	return (
-		<>
-			<Title>Página Principal</Title>
-			<Link href="/sobre">
-				<button>Página Sobre</button>
-			</Link>
-		</>
-	);
+export async function getServerSideProps(context) {
+  try {
+    await clientPromise
+    // `await clientPromise` will use the default database passed in the MONGODB_URI
+    // However you can use another database (e.g. myDatabase) by replacing the `await clientPromise` with the following code:
+    //
+    // `const client = await clientPromise`
+    // `const db = client.db("myDatabase")`
+    //
+    // Then you can execute queries against your database like so:
+    // db.find({}) or any of the MongoDB Node Driver commands
+
+    return {
+      props: { isConnected: true },
+    }
+  } catch (e) {
+    console.error(e)
+    return {
+      props: { isConnected: false },
+    }
+  }
 }
