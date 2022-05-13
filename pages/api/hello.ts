87bf895521b7ac5851fe497429cdf43118bfdf79
Flávio Herculano <flavio.hercullano@gmail.com>
2022-05-13 05:26:28 -0300

    correção

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -2,7 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 export default function hello(req:NextApiRequest, res:NextApiResponse):void{
   res.status(200).json({
-    name:"FullCycle"
+    name: "FullCycle"
   }
   )
 }
--- pages/api/products/[id].ts
+++ pages/api/products/[id].ts
@@ -1,8 +1,8 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default function productShow(
-	req: NextApiRequest,
-	res: NextApiResponse
+	req : NextApiRequest,
+	res : NextApiResponse
 ): void {
 	const { id } = req.query;
 	res.status(200).json({
