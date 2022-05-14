5566f2d0e1a469b0b8320dd0cf1740648f8270a2
Flávio Herculano <flavio.hercullano@gmail.com>
2022-05-13 22:04:51 -0300

    conexão com banco de dados que retorna nome e sobrenome

--- pages/users/index.tsx
+++ pages/users/index.tsx
@@ -1,27 +1,23 @@
-export async function getStaticProps() {
+export async function getStaticProps(){
   const data = await fetch('http://localhost:3000/api/users')
   const usuario = await data.json()
-  // console.log(usuario);
   return {
     props: { usuario }
   }
 }
 
-export default function Usuario({ usuario }) {
+  export default function Usuario({ usuario }){
   return (
     <>
-      <h1>olá mundo</h1>
-      <ul>{usuario.map((user) => (
-        <li key={user._id}> {user._id}</li>
-      ))}</ul>
-      <ul>{usuario.map((user) => (
-        <li key={user._id}> {user.name}</li>
-      ) )}</ul>
-      <ul>{usuario.map((user) => (
-        <li key={user._id}> {user.sobrenome}</li>
-      ))}</ul>
-      
-       
-    </>
-  )
-}
+      <h1>Pessoas</h1>
+      {usuario.map(persona => (
+        <div key={persona._id}>
+          <a>
+            <h3>{persona.name}</h3>
+            <h1>o sobrenome é: {persona.sobrenome}</h1>
+          </a>
+        </div>
+      ))}
+    </> 
+    )
+  }
