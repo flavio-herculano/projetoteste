ad808dd1a7ff415817f676d1ba47b8ae599cd691
Flávio Herculano <flavio.hercullano@gmail.com>
2022-05-14 08:37:50 -0300

    Adicionado Ninjas

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,28 @@
-export default function Home() {
-  return (
-    <>
-      <h1>Hello World!</h1>
-    </>
-  )
-}
+export const getStaticProps = async () => {
+	const url = "https://jsonplaceholder.typicode.com/users";
+
+	const res = await fetch(url);
+	const data = await res.json();
+
+	return {
+		props: { data },
+	};
+};
+
+const Index = ({ data }) => {
+	return (
+		<>
+			<h1>Olá!</h1>
+			{data.map((ninja) => (
+				<div key={ninja.id}>
+					<a>
+						<h3>Nome é:{ninja.name}</h3>
+						<h3>Usuário é: {ninja.username}</h3>
+						<h3>Email é: {ninja.email}</h3>
+					</a>
+				</div>
+			))}
+		</>
+	);
+};
+export default Index;
