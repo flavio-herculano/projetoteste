b0dcd912ac920053c73a26b890be60e1ed6209bf
Flávio Herculano <flavio.hercullano@gmail.com>
2022-05-13 23:11:31 -0300

    reset no app

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,7 @@
-
-import clientPromise from './lib/mongodb'
-
 export default function Home({ isConnected }) {
   return (
     <>
-        {isConnected ? (
-          <h2 className="subtitle">Você está conectado ao MongoDB!</h2>
-        ) : (
-          <h2 className="subtitle">
-            Você não está conectado ao MongoDB!
-          </h2>
-        )}
-    
+      <h1>olá</h1>
     </>
   )
-}
-
-export async function getServerSideProps(context) {
-  try {
-    await clientPromise
-    return {
-      props: { isConnected: true },
-    }
-  } catch (e) {
-    console.error(e)
-    return {
-      props: { isConnected: false },
-    }
-  }
 }
